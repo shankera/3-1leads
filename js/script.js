5284ffa47112ae2d9e495a8828c6b3ebe67b8d04
Arjun Shanker <me@shankera.com>
2017-05-12 14:13:35 -0400

    code refactor, changed css colors, added some stats

--- js/script.js
+++ js/script.js
@@ -3,92 +3,176 @@ $(document).ready(function() {
         url: "http://www.3-1leads.com/seasons.txt",
         success: function(data) {
             var rows = data.split('\n');
-            for (var link of rows) {
-                if (link == "") return;
-                var splitLink = link.split('|');
-                var topLevel = splitLink[0]
-                var sidebar = document.getElementById("somethingObvious");
-                var element = document.createElement('li');
-                var test = document.createElement('a');
-                test.textContent = topLevel;
-                test.setAttribute('class', 'notlink');
-                element.appendChild(test)
-                var div = document.createElement('div');
-                splitLink.shift()
-                var subList = document.createElement('ul');
-                subList.setAttribute('class', 'nav nav-sidebar');
-                for (var item of splitLink) {
-                    var splitItem = item.split(',');
-                    var listItem = document.createElement('li');
-                    var listLink = document.createElement('a');
-                    listLink.textContent = splitItem[0];
-                    listLink.setAttribute('onClick', 'refreshTable(\"' + splitItem[1] + '\")');
-                    listItem.appendChild(listLink);
-                    subList.appendChild(listItem);
-                }
-                div.appendChild(subList);
-                sidebar.appendChild(element);
-                sidebar.appendChild(div);
+            for (var season of rows) {
+                if (season == "") return;
+                var seasonData = season.split('|');
+                var seasonYear = seasonData[0]
+                seasonData.shift()
+                var sidebarItemHeader = createSidebarHeader(seasonYear);
+                var sidebarItemContent = createSidebarItems(seasonData);
+                var sidebarList = document.getElementById("sidebarList");
+                sidebarList.appendChild(sidebarItemHeader);
+                sidebarList.appendChild(sidebarItemContent);
             }
         }
     });
 });
 
-var refreshTable = function(value) {
+var createSidebarHeader = function(seasonYear){
+    var sidebarItem = document.createElement('li');
+    var sidebarItemHeader = document.createElement('a');
+    sidebarItemHeader.textContent = seasonYear;
+    sidebarItemHeader.setAttribute('class', 'notlink');
+    sidebarItem.appendChild(sidebarItemHeader)
+    return sidebarItem
+}
+
+var createSidebarItems = function(seasonData) {
+    var sidebarItemContent = document.createElement('div');
+    var childList = document.createElement('ul');
+    childList.setAttribute('class', 'nav nav-sidebar');
+    for (var dataSource of seasonData) {
+        var dataParts = dataSource.split(',');
+        var childListItem = document.createElement('li');
+        var childListLink = document.createElement('a');
+        childListLink.textContent = dataParts[0];
+        childListLink.setAttribute('onClick', 'loadTable(\"' + dataParts[1] + '\")');
+        childListItem.appendChild(childListLink);
+        childList.appendChild(childListItem);
+    }
+    sidebarItemContent.appendChild(childList);
+    return sidebarItemContent
+}
+
+var loadTable = function(value) {
     $.get(value)
-        .done(function(response) {
-            var contentDiv = document.getElementById('content');
-            contentDiv.innerHTML = "";
-            var allData = response.split('\n')
-            var season = new Array;
-            for (var item of allData) {
-                item = item.split(" ")
-                if (item[0] == "" || item[0] == undefined) {
-
-                } else if (item.length == 1) {
-                    var subSeason = new Array;
-                    subSeason.push(item[0].replace(/-/g, ""));
-                    season.push(subSeason);
-                } else if (item.length == 2) {
-                    var subSeason = new Array;
-                    subSeason.push((item[0] + " " + item[1]).replace(/-/g, ""));
-                    season.push(subSeason);
-                } else {
-                    var month = item[0].toLowerCase()
-                    month = month[0].toUpperCase() + month.slice(1)
-                    var result = new Result(month + " " + item[1] + " " + item[2],
-                        item[3],
-                        item[4],
-                        item[5],
-                        item[6]);
-                    season[season.length - 1].push(result);
-                }
-            }
-
-            for (var part of season) {
-                var header = document.createElement('h3');
-                header.textContent = part[0]
-                content.appendChild(header)
-                part.shift()
-                createTable(part, content)
-            }
-        })
+    .done(function(response) {
+        content.innerHTML = "";
+        var season = parseResponse(response)
+        for (var part of season) {
+            var header = document.createElement('h3');
+            header.textContent = part[0]
+            header.setAttribute('class', 'highlight')
+            content.appendChild(header)
+            part.shift()
+            makeStats(part)
+            createTable(part)
+        }
+    })
 };
 
-var createTable = function(data, content) {
+var makeStats =  function(data) {
+    var totalWins = new Array;
+    var totalLosses = new Array;
+    var blownLeadTeams = [];
+    var comebackTeams = [];
+    var mostComebacks = 0;
+    var mostBlownLeads = 0;
+    for (var result of data){
+        if(result.result == "L"){
+            totalLosses.push(result)
+            if (blownLeadTeams[result.leadingTeam] == undefined){
+                blownLeadTeams[result.leadingTeam] = 0
+            }
+            blownLeadTeams[result.leadingTeam] += 1;
+            if (blownLeadTeams[result.leadingTeam] > mostBlownLeads){
+                mostBlownLeads = blownLeadTeams[result.leadingTeam];
+            }
+            if (comebackTeams[result.trailingTeam] == undefined){
+                comebackTeams[result.trailingTeam] = 0
+            }
+            comebackTeams[result.trailingTeam] += 1;
+            if (comebackTeams[result.trailingTeam] > mostComebacks){
+                mostComebacks = comebackTeams[result.trailingTeam];
+            }
+        } else {
+            totalWins.push(result)
+        }
+    }
+    var retainedLeads = document.createElement('p')
+    retainedLeads.innerHTML = "Total retained 3-1 leads: <span class='highlight'>" + totalWins.length + "</span>";
+    content.appendChild(retainedLeads)
+    var blownLeads = document.createElement('p')
+    blownLeads.innerHTML = "Total blown 3-1 leads: <span class='highlight'>" + totalLosses.length + "</span>";
+    content.appendChild(blownLeads)
+    var percentage = document.createElement('p')
+    percentage.innerHTML = "Percentage of blown 3-1 leads: <span class='highlight'>" + Math.round((totalLosses.length/totalWins.length)*10000)/100 + "%</span>";
+    content.appendChild(percentage)
+    if(mostComebacks != 0) {
+        var mostBlownLeadTeams = "";
+        for(var team in blownLeadTeams){
+            if(blownLeadTeams[team] == mostBlownLeads){
+                if(mostBlownLeadTeams != ""){
+                    mostBlownLeadTeams +=", "
+                }
+                mostBlownLeadTeams += team
+            }
+        }
+        var mostComebackTeams = "";
+        for(var team in comebackTeams){
+            if(comebackTeams[team] == mostComebacks){
+                if(mostComebackTeams != ""){
+                    mostComebackTeams += ", "
+                }
+                mostComebackTeams += team
+            }
+        }
+
+        var teamBlownLeads = document.createElement('p')
+        teamBlownLeads.innerHTML = "Team(s) with the most blown 3-1 leads: <span class='highlight'>" + mostBlownLeadTeams + " (" + mostBlownLeads + ")</span>";
+        content.appendChild(teamBlownLeads)
+        var teamComebacks = document.createElement('p')
+        teamComebacks.innerHTML = "Team(s) with the most 3-1 comebacks: <span class='highlight'>" + mostComebackTeams + " (" + mostComebacks + ")</span>";
+        content.appendChild(teamComebacks)
+    }
+}
+
+var parseResponse = function(response) {
+    var allData = response.split('\n')
+    var season = new Array;
+    for (var item of allData) {
+        item = item.split(" ")
+        if (item[0] == "" || item[0] == undefined) {
+        } else if (item.length == 1) {
+            var subSeason = new Array;
+            subSeason.push(item[0].replace(/-/g, ""));
+            season.push(subSeason);
+        } else if (item.length == 2) {
+            var subSeason = new Array;
+            subSeason.push((item[0] + " " + item[1]).replace(/-/g, ""));
+            season.push(subSeason);
+        } else {
+            var month = item[0].toLowerCase()
+            month = month[0].toUpperCase() + month.slice(1)
+            var result = new Result(month + " " + item[1] + " " + item[2],
+                item[3],
+                item[4],
+                item[5],
+                item[6]);
+            season[season.length - 1].push(result);
+        }
+    }
+    return season;
+}
+
+var createTable = function(data) {
     var table = document.createElement('table');
-    // table.setAttribute('cellspacing', '10')
     var tableHeader = document.createElement('tr');
     var td = document.createElement('th');
     td.textContent = "Date";
+    td.setAttribute('class', 'highlight');
     var tl = document.createElement('th');
     tl.textContent = "Leading Team";
+    tl.setAttribute('class', 'highlight');
     var tr = document.createElement('th');
     tr.textContent = "Result";
+    tr.setAttribute('class', 'highlight');
     var ts = document.createElement('th');
     ts.textContent = "Score";
+    ts.setAttribute('class', 'highlight');
     var tt = document.createElement('th');
     tt.textContent = "Trailing Team";
+    tt.setAttribute('class', 'highlight');
     tableHeader.append(td);
     tableHeader.append(tl);
     tableHeader.append(tr);
